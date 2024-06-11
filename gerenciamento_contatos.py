faa2b6f2d4b252dc2d0b6049b34a27ee10b4bf88
Vicencio <Heloisagoulartv@gmail.com>
2024-06-11 15:55:17 -0300

    Add the definition of Binary Search Tree

--- gerenciamento_contatos.py
+++ gerenciamento_contatos.py
@@ -52,4 +52,66 @@ class LinkedList:
             current = current.next
         print(elems)
 
+
+    # Definição da Árvore Binária de Busca
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = TreeNode(key)
+        else:
+            self._insert(self.root, key)
+
+    def _insert(self, root, key):
+        if root is None:
+            return TreeNode(key)
+        if key < root.val:
+            root.left = self._insert(root.left, key)
+        else:
+            root.right = self._insert(root.right, key)
+        return root
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, root, key):
+        if root is None or root.val == key:
+            return root
+        if key < root.val:
+            return self._search(root.left, key)
+        return self._search(root.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, root, key):
+        if root is None:
+            return root
+        if key < root.val:
+            root.left = self._delete(root.left, key)
+        elif key > root.val:
+            root.right = self._delete(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            temp = self._minValueNode(root.right)
+            root.val = temp.val
+            root.right = self._delete(root.right, temp.val)
+        return root
+
+    def _minValueNode(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
     
